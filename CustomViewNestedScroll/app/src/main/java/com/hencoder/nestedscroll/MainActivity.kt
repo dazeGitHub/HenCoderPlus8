2eea4daa972ef4f7ab85478f22612289fda413c7
mayundaze <1992330573@qq.com>
2022-04-10 16:43:22 +0800

    修改代码

--- CustomViewNestedScroll/app/src/main/java/com/hencoder/nestedscroll/MainActivity.kt
+++ CustomViewNestedScroll/app/src/main/java/com/hencoder/nestedscroll/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.nested_scalable_image_view)
+    setContentView(R.layout.nested_scroll_view) //nested_scalable_image_view //nested_scroll_view
   }
 }

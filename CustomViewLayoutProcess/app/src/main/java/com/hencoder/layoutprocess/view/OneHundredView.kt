38db05a1fb57a473b7bb0e3814aa9cf3a9917eef
dazeGitHub <2711637244@qq.com>
2022-05-23 20:30:23 +0800

    提交

--- CustomViewLayoutProcess/app/src/main/java/com/hencoder/layoutprocess/view/OneHundredView.kt
+++ CustomViewLayoutProcess/app/src/main/java/com/hencoder/layoutprocess/view/OneHundredView.kt
@@ -5,15 +5,15 @@ import android.util.AttributeSet
 import android.view.View
 
 class OneHundredView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
-  override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-    setMeasuredDimension(100, 100)
-  }
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        setMeasuredDimension(100, 100)
+    }
 
-  override fun layout(l: Int, t: Int, r: Int, b: Int) {
-    super.layout(l, t, l + 100, t + 100)
-  }
+    override fun layout(l: Int, t: Int, r: Int, b: Int) {
+        super.layout(l, t, l + 100, t + 100)
+    }
 
-  override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
-    super.onLayout(changed, left, top, right, bottom)
-  }
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+    }
 }
